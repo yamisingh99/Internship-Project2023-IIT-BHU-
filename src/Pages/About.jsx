ce8239e45ae8d01805b8cce4ae9a0ee05011d698
yamisingh99 <yamisinghsmart@gmail.com>
2023-05-21 11:58:18 +0530

    new files

---
+++ src/Pages/About.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const About = () => {
+  return (
+    <div id="About" style={{height:"800px"}}>
+      <h1 style={{fontSize:"50px",padding:"20%"}}>
+        compound search
+      </h1>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/Pages/Projects.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const Projects = () => {
+  return (
+    <div id="Projects" style={{height:"800px"}}>
+      <h1 style={{fontSize:"50px",padding:"20%"}}>
+        new pridiction
+      </h1>
+    </div>
+  );
+};
+
+export default Projects;
